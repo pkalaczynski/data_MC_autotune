2896c133077fd2f9239c3c0f358b13695dac6ade
Piotr Kalaczynski <pkalaczynski@chuck.chuck.camk.edu.pl>
2026-09-05 22:49:14 +0200

    add data downloader script for Higgs ML dataset

---
+++ data/download_HiggsML.sh
@@ -0,0 +1,6 @@
+wget -avP https://zenodo.org/records/15131565/files/FAIR_Universe_HiggsML_data.zip
+unzip FAIR_Universe_HiggsML_data.zip
+echo "finished downloading and unzipping the FAIR Universe Higgs ML dataset"
+echo "Cleaning up the .zip file ..."
+rm FAIR_Universe_HiggsML_data.zip
+echo "done"
